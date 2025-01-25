de8032a66c553591f24b15e31aa4152cdee04f29
JeanVittory <vittory.dev@gmail.com>
2025-01-25 18:48:19 -0500

    feat: :wrench: Add maximum values of load size files as constants variables

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,8 @@ export const EACCES = 'EACCES';
 
 export const FILE_EXTENSION = '.txt';
 export const MULTER_MESSAGE_ERROR_EXT_FILE = 'Only .txt files are allowed!';
+export const MAX_FILE_SIZE_IN_KB = 3 * 1024; // max capability: 3kb
+export const MAX_FILE_SIZE_IN_BYTES = 3072; // max capability: 3kb
 
 export const RELATIVE_ROUTE_TO_LOG_INFO_FILE = '../../logs/info.log';
 export const RELATIVE_ROUTE_TO_LOG_ERROR_FILE = '../../logs/errors.log';
