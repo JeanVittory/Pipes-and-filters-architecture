b0b4dd519543e37cee60af1af496a8daedda08cd
JeanVittory <vittory.dev@gmail.com>
2025-01-25 18:56:43 -0500

    feat: :art: Add formatter to map values from express request file and then use it in the logger information

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+import logger from '../config/logs/winston';
+import { IMulterFile } from '../models/uploadFile';
+
+export const extractFileDetails = (file: IMulterFile | undefined) => {
+  logger.info(`Executing function extractFileDetails...`);
+  return file
+    ? `File Details:
+  - Fieldname: ${file.fieldname || null}
+  - Originalname: ${file.originalname || null}}
+  - Encoding: ${file.encoding || null}}
+  - Mimetype: ${file.mimetype || null}}
+  - Size: ${file.size || null} bytes
+  - Buffer: ${file.buffer.byteLength ? file.buffer.byteLength : 'empty'}`
+    : 'No file uploaded';
+};
