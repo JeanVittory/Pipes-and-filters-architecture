61933e8d358c7baa79ec650976a9d31bd3427761
JeanVittory <vittory.dev@gmail.com>
2025-01-25 18:52:38 -0500

    feat: :loud_sound: Add new logs into multer configuration to monitor events

---
+++ src/middlewares/multer.ts
@@ -0,0 +1,21 @@
+import multer from 'multer';
+import path from 'path';
+import { FILE_EXTENSION, MULTER_MESSAGE_ERROR_EXT_FILE } from '../constants';
+import logger from '../config/logs/winston';
+
+logger.info(`Executing multerMiddleware file...`);
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname.toLowerCase());
+    if (ext !== FILE_EXTENSION)
+      return cb(new Error(MULTER_MESSAGE_ERROR_EXT_FILE));
+    return cb(null, true);
+  },
+});
+
+logger.info(`End multerMiddleware file`);
+
+export default upload.single('file');
