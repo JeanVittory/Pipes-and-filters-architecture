c9802308a02189a008a9275ea918484f589cc389
JeanVittory <vittory.dev@gmail.com>
2025-01-25 15:19:38 -0500

    feat: :wrench: Add a server configuration file

---
+++ src/config/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { EACCES, EADDRINUSE, PORT, PREFIX_API } from '../constants';
+import router from '../routes';
+
+function init() {
+  const app = express();
+
+  app.use(PREFIX_API, router);
+
+  const server = app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
+
+  server.on('error', (error) => {
+    if (error.name === EADDRINUSE) {
+      console.log(`Port ${PORT} is already in use.`);
+    } else if (error.name === EACCES) {
+      console.log(`Insufficient privileges to bind to port ${PORT}.`);
+    } else {
+      console.error(`An error occurred: ${error.message}`);
+    }
+    process.exit(1);
+  });
+}
+
+export default init;
