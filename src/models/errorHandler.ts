c01fb249e75a6bfe45783bc7abd0be314298f802
JeanVittory <vittory.dev@gmail.com>
2025-01-25 17:29:46 -0500

    feat: :wrench: Add enums to centralize error codes and messages

---
+++ src/models/errorHandler.ts
@@ -0,0 +1,8 @@
+export enum BadRequest {
+  code = 400,
+  message = 'Request body could not be read properly.',
+}
+export enum InternalServerError {
+  code = 500,
+  message = 'Internal server error occurred.',
+}
