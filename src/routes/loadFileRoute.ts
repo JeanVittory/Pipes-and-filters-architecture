27a806e8b08bd11c69014977a83b193bc9c6217b
JeanVittory <vittory.dev@gmail.com>
2025-01-25 18:54:57 -0500

    feat: :wrench: Add uploadFileMiddleware into loadFileRoute to validate information

--- src/routes/loadFileRoute.ts
+++ src/routes/loadFileRoute.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { loadFileController } from '../controllers';
-import multerMiddleware from '../config/middlewares/multer';
+import multerMiddleware from '../middlewares/multer';
+import { uploadFileMiddleware } from '../middlewares/uploadFileMiddleware';
 
 const loadFileRoute = Router();
 
-loadFileRoute.post('/', multerMiddleware, loadFileController);
+loadFileRoute.post(
+  '/',
+  multerMiddleware,
+  uploadFileMiddleware,
+  loadFileController
+);
 
 export default loadFileRoute;
