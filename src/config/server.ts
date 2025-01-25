1549e7e24edaabc18bc43c6146abf2f9af9fcaea
JeanVittory <vittory.dev@gmail.com>
2025-01-25 18:50:53 -0500

    feat: :loud_sound: Add new logs at server file to monitor events

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { EACCES, EADDRINUSE, PORT, PREFIX_API } from '../constants';
 import router from '../routes';
-import { errorHandler } from './middlewares/errorHandler';
+import { errorHandler } from '../middlewares/errorHandler';
+import logger from './logs/winston';
 
 function init() {
   const app = express();
@@ -11,16 +12,16 @@ function init() {
   app.use(errorHandler);
 
   const server = app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+    logger.info(`Server running at port ${PORT}`);
   });
 
   server.on('error', (error) => {
     if (error.name === EADDRINUSE) {
-      console.log(`Port ${PORT} is already in use.`);
+      logger.error(`Port ${PORT} is already in use.`);
     } else if (error.name === EACCES) {
-      console.log(`Insufficient privileges to bind to port ${PORT}.`);
+      logger.error(`Insufficient privileges to bind to port ${PORT}.`);
     } else {
-      console.error(`An error occurred: ${error.message}`);
+      logger.error(`An error occurred: ${error.message}`);
     }
     process.exit(1);
   });
