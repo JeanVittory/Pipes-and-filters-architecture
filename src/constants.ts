328fa697dddbabc89e2f2bb9f02fffcabfcba71f
JeanVittory <vittory.dev@gmail.com>
2025-01-26 17:00:41 -0500

    feat: :wrench: Add Regex as constants to reutilize them across the project

--- src/constants.ts
+++ src/constants.ts
@@ -11,3 +11,8 @@ export const MAX_FILE_SIZE_IN_BYTES = 3072; // max capability: 3kb
 
 export const RELATIVE_ROUTE_TO_LOG_INFO_FILE = '../../logs/info.log';
 export const RELATIVE_ROUTE_TO_LOG_ERROR_FILE = '../../logs/errors.log';
+
+export const REGEX_ONLY_NUMBERS_AND_LETTERS = /[^a-zA-Z0-9áéíóúÁÉÍÓÚ\s\n]/g;
+export const REGEX_REMOVE_WHITESPACE = /[ ]{2,}/g;
+export const REGEX_REMOVE_WHITESPACE_AT_NEWLINE = /^[ ]+/gm;
+export const REGEX_REMOVE_NEW_LINES = /\n{2,}/g;
