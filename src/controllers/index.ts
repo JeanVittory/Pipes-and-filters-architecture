37f2f49b6ea8498be007aecad135ffb2fb00bace
JeanVittory <vittory.dev@gmail.com>
2025-01-26 17:04:28 -0500

    feat: :wrench: Add the source filter within loadFileController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,10 @@
 import { Request, Response } from 'express';
+import logger from '../config/logs/winston';
+import { initializeProcess } from '../service/initializeProcess';
 
-export const loadFileController = (req: Request, res: Response) => {};
+export const loadFileController = (req: Request, res: Response) => {
+  logger.info(`Executing function loadFileController...`);
+  const { file } = req;
+  const fileContent = file?.buffer.toString('utf-8');
+  initializeProcess(fileContent!);
+};
