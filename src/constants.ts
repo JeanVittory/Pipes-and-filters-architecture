ca884c2280a9193babc59ed687e34c67a6614e0a
JeanVittory <vittory.dev@gmail.com>
2025-01-26 21:16:19 -0500

    feat: :art: Add keywords related to categories filter using a Map as data structure

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { CATEGORIES } from './models/categories';
+
 export const PORT = 4000;
 export const PREFIX_API = '/api';
 
@@ -11,8 +13,16 @@ export const MAX_FILE_SIZE_IN_BYTES = 3072; // max capability: 3kb
 
 export const RELATIVE_ROUTE_TO_LOG_INFO_FILE = '../../logs/info.log';
 export const RELATIVE_ROUTE_TO_LOG_ERROR_FILE = '../../logs/errors.log';
+export const RELATIVE_ROUTE_TO_LOG_VERBOSE_FILE = '../../logs/verbose.log';
 
 export const REGEX_ONLY_NUMBERS_AND_LETTERS = /[^a-zA-Z0-9áéíóúÁÉÍÓÚ\s\n]/g;
 export const REGEX_REMOVE_WHITESPACE = /[ ]{2,}/g;
 export const REGEX_REMOVE_WHITESPACE_AT_NEWLINE = /^[ ]+/gm;
 export const REGEX_REMOVE_NEW_LINES = /\n{2,}/g;
+
+export const KEYWORDS_BY_CATEGORY = new Map<CATEGORIES, string[]>([
+  [CATEGORIES.URGENT, ['urgente', 'importante', 'prioridad']],
+  [CATEGORIES.IRFORMATION, ['información', 'detalles', 'documentación']],
+  [CATEGORIES.ADVERTISING, ['oferta', 'descuento', 'promoción']],
+  [CATEGORIES.UNKNOWN, []],
+]);
