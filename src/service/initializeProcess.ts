38ec83460313d21a14113d8474de13d3f250281b
JeanVittory <vittory.dev@gmail.com>
2025-01-27 18:35:44 -0500

    feat: :wrench: Update the initial pipe with the classifierFilter implementation.

--- src/service/initializeProcess.ts
+++ src/service/initializeProcess.ts
@@ -1,8 +1,10 @@
 import logger from '../config/logs/winston';
+import { classifierFilter } from '../filters/classifierFilter';
 import { cleanerFilter } from '../filters/cleanerFilter';
 import { pipe } from '../pipes/pipe';
 
 export const initializeProcess = (stream: string) => {
   logger.verbose('Executing initializeProcess function...');
-  const result = pipe(stream, [cleanerFilter]);
+  const result = pipe(stream, [cleanerFilter, classifierFilter]);
+  console.log('result', result);
 };
