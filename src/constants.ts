beb2b44647c7447304a71cb013ef08d50c289180
JeanVittory <vittory.dev@gmail.com>
2025-01-27 20:31:45 -0500

    feat: :art: Update REGEX_REMOVE_WHITESPACE_AT_NEWLINE

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ export const RELATIVE_ROUTE_TO_LOG_VERBOSE_FILE = '../../logs/verbose.log';
 
 export const REGEX_ONLY_NUMBERS_AND_LETTERS = /[^a-zA-Z0-9áéíóúÁÉÍÓÚ\s\n]/g;
 export const REGEX_REMOVE_WHITESPACE = /[ ]{2,}/g;
-export const REGEX_REMOVE_WHITESPACE_AT_NEWLINE = /^[ ]+/gm;
+export const REGEX_REMOVE_WHITESPACE_AT_NEWLINE = /^[ ]+|[ ]+$/gm;
 export const REGEX_REMOVE_NEW_LINES = /\n{2,}/g;
 
 export const KEYWORDS_BY_CATEGORY = new Map<CATEGORIES, string[]>([
