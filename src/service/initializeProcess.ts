45b0e8e4597a2a1f1f7969048cb5cc605ce3d178
JeanVittory <vittory.dev@gmail.com>
2025-01-27 20:40:30 -0500

    feat: :sparkles: Add a new filter call it counterFilter at the initial pipe

--- src/service/initializeProcess.ts
+++ src/service/initializeProcess.ts
@@ -1,10 +1,11 @@
 import logger from '../config/logs/winston';
 import { classifierFilter } from '../filters/classifierFilter';
 import { cleanerFilter } from '../filters/cleanerFilter';
+import { counterFilter } from '../filters/counterFilter';
 import { pipe } from '../pipes/pipe';
 
 export const initializeProcess = (stream: string) => {
   logger.verbose('Executing initializeProcess function...');
-  const result = pipe(stream, [cleanerFilter, classifierFilter]);
+  const result = pipe(stream, [cleanerFilter, classifierFilter, counterFilter]);
   console.log('result', result);
 };
