9cb07f26779502abfb7380fb8610d359753fc10d
JeanVittory <vittory.dev@gmail.com>
2025-01-30 20:44:49 -0500

    feat: :art: Add square brackets to the highlight response text

--- src/filters/urgentRoute/highlightFilter.ts
+++ src/filters/urgentRoute/highlightFilter.ts
@@ -5,7 +5,7 @@ export const highlightFilter = (stream: IFilterResult): IFilterResult => {
   const { text, filtersApplied } = stream;
   return {
     ...stream,
-    text: `${HIGHLIGHT_URGENT} ${text}`,
+    text: `[${HIGHLIGHT_URGENT}] ${text}`,
     filtersApplied: [...filtersApplied, 'highligh filter'],
   };
 };
