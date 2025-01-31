78ba2f79c702ecac83a93a9861944a707ad6ffdb
JeanVittory <vittory.dev@gmail.com>
2025-01-30 20:49:43 -0500

    feat: :construction: Add the urgent route to the router for testing purposes.

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,6 @@
 import logger from '../config/logs/winston';
 import { highlightFilter } from '../filters/urgentRoute/highlightFilter';
+import { uppercaseFilter } from '../filters/urgentRoute/uppercaseFilter';
 import { CATEGORIES_TYPE } from '../models/categories';
 import { IFilterResult } from '../models/filters';
 import { pipe } from '../pipes/pipe';
@@ -7,18 +8,18 @@ import { pipe } from '../pipes/pipe';
 const urgentRoute = (stream: IFilterResult): IFilterResult => {
   logger.verbose('Executing urgentRoute function...');
 
-  return pipe(stream, [highlightFilter]);
+  return pipe(stream, [highlightFilter, uppercaseFilter]);
 };
 
 const routerMap = {
   urgente: urgentRoute,
-  informacion: '',
-  publicidad: '',
-  otros: '',
 };
 
 export const router = (stream: IFilterResult) => {
   logger.verbose('Executing router function...');
 
-  const result = routerMap[stream.category as CATEGORIES_TYPE];
+  const route = routerMap[stream.category as CATEGORIES_TYPE];
+  const result = route(stream);
+
+  console.log(result.text);
 };
