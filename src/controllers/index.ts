59db148dbd4e0132d5044d42c3e69e1c57436cf6
JeanVittory <vittory.dev@gmail.com>
2025-02-01 17:14:05 -0500

    feat: :loud_sound: Update the logs that track the controller's flow.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from 'express';
 import logger from '../config/logs/winston';
 import { initializeProcess } from '../service/initializeProcess';
+import { DECODE_FORMAT } from '../constants';
 
 export const loadFileController = (req: Request, res: Response) => {
   logger.verbose(`Executing function loadFileController...`);
   const { file } = req;
-  const fileContent = file?.buffer.toString('utf-8');
+
+  logger.info(`Processing file: ${file?.filename}`);
+
+  const fileContent = file?.buffer.toString(DECODE_FORMAT);
   const dataProcessed = initializeProcess(fileContent!);
 
+  logger.info(`Applied filters: ${dataProcessed.filtersApplied.join(', ')}`);
+
+  logger.info(`Category: ${dataProcessed.category}`);
+
+  logger.info(`Summary: 
+    Words: ${dataProcessed.counter?.words},
+    Lines: ${dataProcessed.counter?.lines},
+    Characters: ${dataProcessed.counter?.characters}
+  `);
+
   logger.verbose(`Finishing function loadFileController...`);
+
   res.status(200).json(dataProcessed);
 };
