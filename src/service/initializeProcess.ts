490c1880c14cf54c9d4c2eb6f59b0778c10884f8
JeanVittory <vittory.dev@gmail.com>
2025-01-31 22:44:43 -0500

    feat: :sparkles: Implement the router and return its result, logging some messages throughout the process

--- src/service/initializeProcess.ts
+++ src/service/initializeProcess.ts
@@ -3,14 +3,20 @@ import { classifierFilter } from '../filters/classifierFilter';
 import { cleanerFilter } from '../filters/cleanerFilter';
 import { counterFilter } from '../filters/counterFilter';
 import { pipe } from '../pipes/pipe';
-import { router } from '../router/router';
+import { router } from '../routerFilter/router';
 
 export const initializeProcess = (stream: string) => {
   logger.verbose('Executing initializeProcess function...');
+
   const dataCleaned = pipe(stream, [
     cleanerFilter,
     classifierFilter,
     counterFilter,
   ]);
-  router(dataCleaned);
+
+  const dataFromRouter = router(dataCleaned);
+
+  logger.verbose('Finishing initializeProcess function...');
+
+  return dataFromRouter;
 };
