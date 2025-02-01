e359c6d5f35deaa4d2a5d078dac5de3a051ee3ea
JeanVittory <vittory.dev@gmail.com>
2025-02-01 17:11:12 -0500

    feat: :safety_vest: Add a new regex to validate the different formats of the text that will be processed.

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,8 @@ export const PREFIX_API = '/api';
 export const EADDRINUSE = 'EADDRINUSE';
 export const EACCES = 'EACCES';
 
+export const DECODE_FORMAT = 'utf-8';
+
 export const FILE_EXTENSION = '.txt';
 export const MULTER_MESSAGE_ERROR_EXT_FILE = 'Only .txt files are allowed!';
 export const MAX_FILE_SIZE_IN_KB = 3 * 1024; // max capability: 3kb
@@ -19,6 +21,9 @@ export const REGEX_ONLY_NUMBERS_AND_LETTERS = /[^a-zA-Z0-9áéíóúÁÉÍÓÚ\s
 export const REGEX_REMOVE_WHITESPACE = /[ ]{2,}/g;
 export const REGEX_REMOVE_WHITESPACE_AT_NEWLINE = /^[ ]+|[ ]+$/gm;
 export const REGEX_REMOVE_NEW_LINES = /\n{2,}/g;
+export const ONLY_CONTAINS_SYMBOLS = /^[^a-zA-Z0-9\s\n]+$/gm;
+export const ONLY_CONTAINS_NUMBERS_AND_SPECIAL_CHARACTERS =
+  /^[\d\s.,!@#$%^&*()_+=\-[\]{};':"\\|,.<>/?\n]*$/;
 
 export const KEYWORDS_BY_CATEGORY = new Map<CATEGORIES, string[]>([
   [CATEGORIES.URGENT, ['urgente', 'importante', 'prioridad']],
